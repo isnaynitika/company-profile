ded109fc13a8e7e63112169138dc70d20471434e
muhammadfihan <muhammadfihan@192.168.58.1>
2024-03-04 13:02:05 +0700

    Update

--- src/components/DataSolution/index.js
+++ src/components/DataSolution/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import VisibleComponent from "../Visible";
+import Link from "next/link";
 
 const ItemSolution = ({ gambarfitur, judulgambar, descjudul, index }) => {
   const isEvenRow = index % 2 === 0;
@@ -41,27 +42,26 @@ const ItemSolution = ({ gambarfitur, judulgambar, descjudul, index }) => {
           {descjudul}
         </p>
         <div className="flex flex-row mt-3 md:mt-10 md:gap-x-4">
-          <button
-            href="#"
-            className="inline-flex justify-center items-center p-2 px-3 text-base font text-center text-white rounded-xl bg-red-500 hover:bg-red-800 font-light"
-          >
-            Konsultasi
-            <svg
-              className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </button>
+          <Link href="/hubungi-kami">
+            <button className="inline-flex justify-center items-center p-2 px-3 text-base font text-center text-white rounded-xl bg-red-500 hover:bg-red-800 font-light">
+              Konsultasi
+              <svg
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
